3f47c1ba2def2ec10dc6dc8a14a18f4efb891f5f
Haima-santhosh <hymasanthosh14@gmail.com>
2025-07-15 13:25:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="h-screen flex justify-center items-center text-red-700 bg-slate-500">Hello From Food App</h1>
+      
+    </>
+  )
+}
+
+export default App
